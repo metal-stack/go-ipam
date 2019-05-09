78d1400130d4105d72102af79c044f8dddfa5e72
Stefan Majer <stefan.majer@f-i-ts.de>
2019-05-09 10:28:19 +0200

    Add package doc and example

---
+++ doc.go
@@ -0,0 +1,38 @@
+/*
+Package go-ipam is a ip address management library for ip's and prefixes (networks).
+
+It uses either memory or postgresql database to store the ip's and prefixes.
+You can also bring you own Storage implementation as you need.
+
+Example usage:
+
+		import (
+			"fmt"
+			goipam "github.com/metal-pod/go-ipam"
+		)
+
+
+		func main() {
+			// create a ipamer with in memory storage
+			ipam := goipam.New()
+
+			prefix, err := ipam.NewPrefix("192.168.0.0/24")
+			if err != nil {
+				panic(err)
+			}
+
+			ip, err := ipam.AcquireIP(prefix)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("got IP: %s", ip.IP)
+
+			err = ipam.ReleaseIP(ip)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("IP: %s released.", ip.IP)
+		}
+
+*/
+package ipam
--- ipam_test.go
+++ ipam_test.go
@@ -1,6 +1,7 @@
 package ipam
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -17,4 +18,47 @@ func Test_New(t *testing.T) {
 	ipamer := New()
 	require.NotNil(t, ipamer)
 	require.NotNil(t, ipamer.storage)
+}
+
+func ExampleIpamer_NewPrefix() {
+	ipamer := New()
+	prefix, err := ipamer.NewPrefix("192.168.0.0/24")
+	if err != nil {
+		panic(err)
+	}
+	ip1, err := ipamer.AcquireIP(prefix)
+	if err != nil {
+		panic(err)
+	}
+	ip2, err := ipamer.AcquireIP(prefix)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(prefix)
+	fmt.Println(ip1.IP.String())
+	fmt.Println(ip1.ParentPrefix)
+	fmt.Println(ip2.IP.String())
+	fmt.Println(ip2.ParentPrefix)
+	// Output:
+	// 192.168.0.0/24
+	// 192.168.0.1
+	// 192.168.0.0/24
+	// 192.168.0.2
+	// 192.168.0.0/24
+
+	err = ipamer.ReleaseIP(ip2)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ipamer.ReleaseIP(ip1)
+	if err != nil {
+		panic(err)
+	}
+	_, err = ipamer.DeletePrefix(prefix.Cidr)
+	if err != nil {
+		panic(err)
+	}
+
 }
