d3ad4a4aa9924a8911068fcb7a86f64b599d10cf
Stefan Majer <stefan.majer@f-i-ts.de>
2019-05-10 10:08:23 +0200

    Try with bigger network

--- prefix_benchmark_test.go
+++ prefix_benchmark_test.go
@@ -31,7 +31,7 @@ func BenchmarkNewPrefixPostgres(b *testing.B) {
 }
 
 func benchmarkAquireIP(ipam *Ipamer, b *testing.B) {
-	p, err := ipam.NewPrefix("192.168.0.0/24")
+	p, err := ipam.NewPrefix("10.0.0.0/8")
 	if err != nil {
 		panic(err)
 	}
