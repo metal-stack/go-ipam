81cbab894364a01aa9d0f9a152665c2ff4044f35
Alexander Bloss <alexander.bloss@f-i-ts.de>
2019-08-08 14:25:09 +0200

    reproduced error case introduced typed OptimisticLockError implemented retry on OptimisticLockError for relevant methods

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/metal-pod/go-ipam
 go 1.12
 
 require (
+	github.com/avast/retry-go v2.4.1+incompatible
 	github.com/go-sql-driver/mysql v1.4.1 // indirect
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/lib/pq v1.1.1
 	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/stretchr/testify v1.3.0
 	google.golang.org/appengine v1.6.0 // indirect
 )
--- storage.go
+++ storage.go
@@ -8,3 +8,17 @@ type Storage interface {
 	UpdatePrefix(prefix *Prefix) (*Prefix, error)
 	DeletePrefix(prefix *Prefix) (*Prefix, error)
 }
+
+// OptimisticLockError indicates that the operation could not be executed because the dataset to update has changed in the meantime.
+// clients can decide to read the current dataset and retry the operation.
+type OptimisticLockError struct {
+	msg string
+}
+
+func (o OptimisticLockError) Error() string {
+	return o.msg
+}
+
+func NewOptimisticLockError(msg string) OptimisticLockError {
+	return OptimisticLockError{msg: msg}
+}
