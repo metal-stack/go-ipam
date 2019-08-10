50091734826f480513f15f2f380baab511862331
droid42 <info@droid42.de>
2019-08-10 13:11:37 +0200

    Address issue #13 added lock for memory-storage map

--- memory.go
+++ memory.go
@@ -2,22 +2,28 @@ package ipam
 
 import (
 	"fmt"
+	"sync"
 )
 
 type memory struct {
 	prefixes map[string]*Prefix
+	lock     sync.RWMutex
 }
 
 // NewMemory create a memory storage for ipam
 func NewMemory() *memory {
 	prefixes := make(map[string]*Prefix)
+	lock := sync.RWMutex{}
 	return &memory{
 		prefixes: prefixes,
+		lock:     lock,
 	}
-
 }
 
 func (m *memory) CreatePrefix(prefix *Prefix) (*Prefix, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
 	_, ok := m.prefixes[prefix.Cidr]
 	if ok {
 		return nil, fmt.Errorf("prefix already created:%v", prefix)
@@ -26,9 +32,15 @@ func (m *memory) CreatePrefix(prefix *Prefix) (*Prefix, error) {
 	return prefix, nil
 }
 func (m *memory) ReadPrefix(prefix string) (*Prefix, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	return m.prefixes[prefix], nil
 }
 func (m *memory) ReadAllPrefixes() ([]*Prefix, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
 	ps := make([]*Prefix, 0, len(m.prefixes))
 	for _, v := range m.prefixes {
 		ps = append(ps, v)
@@ -36,6 +48,9 @@ func (m *memory) ReadAllPrefixes() ([]*Prefix, error) {
 	return ps, nil
 }
 func (m *memory) UpdatePrefix(prefix *Prefix) (*Prefix, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
 	if prefix.Cidr == "" {
 		return nil, fmt.Errorf("prefix not present:%v", prefix)
 	}
@@ -47,6 +62,9 @@ func (m *memory) UpdatePrefix(prefix *Prefix) (*Prefix, error) {
 	return prefix, nil
 }
 func (m *memory) DeletePrefix(prefix *Prefix) (*Prefix, error) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
 	delete(m.prefixes, prefix.Cidr)
 	return prefix, nil
 }
--- memory_test.go
+++ memory_test.go
@@ -1,6 +1,7 @@
 package ipam
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -20,4 +21,47 @@ func Test_UpdatePrefix(t *testing.T) {
 	require.NotNil(t, err)
 	require.Nil(t, p)
 	require.Equal(t, "prefix not found:1.2.3.4/24", err.Error())
+}
+
+// ensure that locks on memory storage work
+func Test_UpdatePrefix_Concurrent(t *testing.T) {
+	m := NewMemory()
+
+	for i := 0; i < 50000; i++ {
+
+		go func(run int) {
+			prefix := &Prefix{}
+			cidr := calcPrefix24(run) + "/24"
+			prefix.Cidr = cidr
+
+			p, err := m.CreatePrefix(prefix)
+			require.Nil(t, err)
+			require.NotNil(t, p)
+
+			p, err = m.ReadPrefix(cidr)
+			require.Nil(t, err)
+			require.NotNil(t, p)
+
+			p, err = m.UpdatePrefix(p)
+			require.Nil(t, err)
+			require.NotNil(t, p)
+
+			p, err = m.ReadPrefix(cidr)
+			require.Nil(t, err)
+			require.NotNil(t, p)
+
+			p, err = m.DeletePrefix(p)
+			require.Nil(t, err)
+			require.NotNil(t, p)
+		}(i)
+	}
+}
+
+// calcs distinct /24 prefix for given test run
+func calcPrefix24(run int) string {
+	i3 := run % 256
+	i2 := (run / 256) % 256
+	i1 := (run / 65536) % 256
+
+	return fmt.Sprintf("%d.%d.%d.0", i1, i2, i3)
 }
