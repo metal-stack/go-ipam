a48861579a1180269d253e9d4cfad78f8b59f986
Krzysztof Nazarewski <gpg@kdn.im>
2023-08-24 11:34:19 +0200

    file: merge refresh() into reload(), improve docs & errors

--- file.go
+++ file.go
@@ -13,11 +13,17 @@ import (
 )
 
 type file struct {
-	path       string
+	// path location of the state file
+	path string
+	// prettyJSON is always true, but up for being configurable
 	prettyJSON bool
-	modTime    time.Time
-	parent     Storage
-	lock       sync.RWMutex
+	// modTime helps with tracking external file changes
+	// usages of modTime will be deprecated after implementing filesystem-level locking
+	modTime time.Time
+	// parent implements internal state management, currently it is always NewMemory()
+	parent Storage
+	// lock at some point should be replaced with filesystem lock
+	lock sync.RWMutex
 }
 
 var (
@@ -25,6 +31,7 @@ var (
 	DefaultLocalFilePath string
 )
 
+// fileJSONData is a representation of JSON file's structure
 type fileJSONData map[string]map[string]prefixJSON
 
 func init() {
@@ -32,6 +39,10 @@ func init() {
 	DefaultLocalFilePath = path.Join(getXDGDataHome(), "go-ipam", "ipam-db.json")
 }
 
+// getXDGDataHome() is an utility for finding the most suitable data directory:
+// 1) $XDG_DATA_HOME
+// 2) $HOME/.local/share
+// 3) current directory
 func getXDGDataHome() string {
 	if val := os.Getenv("XDG_DATA_HOME"); val != "" {
 		return val
@@ -57,32 +68,28 @@ func NewLocalFile(ctx context.Context, path string) Storage {
 	}
 }
 
+// clearParent() empties the internal state
 func (f *file) clearParent(ctx context.Context) (err error) {
 	namespaces, err := f.parent.ListNamespaces(ctx)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to list namespaces: %w", err)
 	}
 	for _, namespace := range namespaces {
 		if err = f.parent.DeleteAllPrefixes(ctx, namespace); err != nil {
-			return err
+			return fmt.Errorf("failed to delete prefixes for %s namespace: %w", namespace, err)
 		}
 		if namespace == defaultNamespace {
 			// skip deletion instead of replicating NewMemory behavior
 			continue
 		}
 		if err = f.parent.DeleteNamespace(ctx, namespace); err != nil {
-			return err
+			return fmt.Errorf("failed to delete %s namespace: %w", namespace, err)
 		}
 	}
-	return err
+	return nil
 }
 
-func (f *file) refresh(ctx context.Context) error {
-	if modTime := f.getModTime(); modTime != nullModTime && modTime == f.modTime {
-		return nil
-	}
-	return f.reload(ctx)
-}
+// getModTime() returns file's modification time without failure (defaulting to nullModTime).
 func (f *file) getModTime() time.Time {
 	info, err := os.Stat(f.path)
 	if err != nil {
@@ -91,8 +98,17 @@ func (f *file) getModTime() time.Time {
 	return info.ModTime()
 }
 
-// see ipamer.NamespacedLoad for similar, but incomplete functionality
+// reload() is meant to synchronize from the file to internal state representation,
+// currently it bases decision solely on the modification time of the file
+//
+// see ipamer.NamespacedLoad for alternative implementation candidate
+// after https://github.com/metal-stack/go-ipam/issues/111 is addressed
 func (f *file) reload(ctx context.Context) (err error) {
+	// don't do anything when file modification time didn't changed
+	if modTime := f.getModTime(); modTime != nullModTime && modTime == f.modTime {
+		return nil
+	}
+
 	var data []byte
 	storage := make(fileJSONData)
 	if _, err = os.Stat(f.path); !errors.Is(err, fs.ErrNotExist) {
@@ -109,24 +125,26 @@ func (f *file) reload(ctx context.Context) (err error) {
 			return fmt.Errorf("failed to parse state file %q: %w", f.path, err)
 		}
 	}
-	// TODO: improve by diffing parent storage instead of discarding and recreating?
 	if err = f.clearParent(ctx); err != nil {
 		return fmt.Errorf("failed to clear memory storage: %w", err)
 	}
 	for namespace, prefixes := range storage {
 		if err = f.parent.CreateNamespace(ctx, namespace); err != nil {
-			return err
+			return fmt.Errorf("failed to reload a %s namespace: %w", namespace, err)
 		}
 		for _, prefix := range prefixes {
 			if _, err = f.parent.CreatePrefix(ctx, prefix.toPrefix(), namespace); err != nil {
-				return err
+				return fmt.Errorf("failed to reload a %s prefix in %s namespace: %w", prefix.Cidr, namespace, err)
 			}
 		}
 	}
-	return err
+	return nil
 }
 
-// see ipamer.NamespacedDump for similar, but incomplete functionality
+// persist() dumps current internal state to file
+//
+// see ipamer.NamespacedDump for alternative implementation candidate
+// after https://github.com/metal-stack/go-ipam/issues/111 is addressed
 func (f *file) persist(ctx context.Context) (err error) {
 	storage := make(fileJSONData)
 	var (
@@ -137,7 +155,7 @@ func (f *file) persist(ctx context.Context) (err error) {
 
 	namespaces, err := f.parent.ListNamespaces(ctx)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to list namespaces while building external state representation: %w", err)
 	}
 	for _, namespace := range namespaces {
 		if prefixes, ok = storage[namespace]; !ok {
@@ -146,7 +164,7 @@ func (f *file) persist(ctx context.Context) (err error) {
 		}
 		ps, err := f.parent.ReadAllPrefixes(ctx, namespace)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to read prefixes of %s namespace while building external state representation: %w", namespace, err)
 		}
 		for _, prefix := range ps {
 			prefixes[prefix.Cidr] = prefix.toPrefixJSON()
@@ -158,7 +176,7 @@ func (f *file) persist(ctx context.Context) (err error) {
 		data, err = json.Marshal(storage)
 	}
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to serialize JSON: %w", err)
 	}
 	err = os.WriteFile(f.path, data, 0600)
 	if err != nil {
@@ -189,7 +207,7 @@ func (f *file) CreatePrefix(ctx context.Context, prefix Prefix, namespace string
 func (f *file) ReadPrefix(ctx context.Context, prefix, namespace string) (p Prefix, err error) {
 	f.lock.RLock()
 	defer f.lock.RUnlock()
-	if err = f.refresh(ctx); err != nil {
+	if err = f.reload(ctx); err != nil {
 		return p, err
 	}
 	return f.parent.ReadPrefix(ctx, prefix, namespace)
@@ -212,7 +230,7 @@ func (f *file) ReadAllPrefixes(ctx context.Context, namespace string) (ps Prefix
 	f.lock.RLock()
 	defer f.lock.RUnlock()
 
-	if err = f.refresh(ctx); err != nil {
+	if err = f.reload(ctx); err != nil {
 		return ps, err
 	}
 	return f.parent.ReadAllPrefixes(ctx, namespace)
@@ -222,7 +240,7 @@ func (f *file) ReadAllPrefixCidrs(ctx context.Context, namespace string) (cidrs 
 	f.lock.RLock()
 	defer f.lock.RUnlock()
 
-	if err = f.refresh(ctx); err != nil {
+	if err = f.reload(ctx); err != nil {
 		return cidrs, err
 	}
 	return f.parent.ReadAllPrefixCidrs(ctx, namespace)
@@ -269,7 +287,7 @@ func (f *file) CreateNamespace(ctx context.Context, namespace string) (err error
 func (f *file) ListNamespaces(ctx context.Context) (result []string, err error) {
 	f.lock.Lock()
 	defer f.lock.Unlock()
-	if err = f.refresh(ctx); err != nil {
+	if err = f.reload(ctx); err != nil {
 		return result, err
 	}
 	return f.parent.ListNamespaces(ctx)
