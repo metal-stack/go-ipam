b252aca3e8ea3e59794d215bdfd879563a6d0749
Stefan Majer <stefan.majer@f-i-ts.de>
2024-03-17 13:58:10 +0100

    Enable grpcurl list

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -94,6 +94,10 @@ func (s *server) Run() error {
 		grpcreflect.NewStaticReflector(apiv1connect.IpamServiceName),
 		compress.WithAll(compress.LevelBalanced),
 	))
+	mux.Handle(grpcreflect.NewHandlerV1Alpha(
+		grpcreflect.NewStaticReflector(apiv1connect.IpamServiceName),
+		compress.WithAll(compress.LevelBalanced),
+	))
 
 	server := http.Server{
 		Addr: s.c.GrpcServerEndpoint,
