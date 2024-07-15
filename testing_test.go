1503103929db22d1d8ee4b0f12e9e408d1c787b1
Stefan Majer <stefan.majer@f-i-ts.de>
2024-07-15 13:51:32 +0200

    Update dependencies (#155)

--- testing_test.go
+++ testing_test.go
@@ -57,7 +57,7 @@ func TestMain(m *testing.M) {
 	}
 	etcdVersion = os.Getenv("ETCD_VERSION")
 	if etcdVersion == "" {
-		etcdVersion = "v3.5.12"
+		etcdVersion = "v3.5.14"
 	}
 	mdbVersion = os.Getenv("MONGODB_VERSION")
 	if mdbVersion == "" {
