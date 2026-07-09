d486271f868b4db8316508c3532040add1b862de
Stefan Majer <stefan.majer@f-i-ts.de>
2026-07-09 08:01:46 +0200

    Update etcd and actions

--- testing_test.go
+++ testing_test.go
@@ -57,7 +57,7 @@ func TestMain(m *testing.M) {
 	}
 	etcdVersion = os.Getenv("ETCD_VERSION")
 	if etcdVersion == "" {
-		etcdVersion = "v3.6.9"
+		etcdVersion = "v3.7.0"
 	}
 	mdbVersion = os.Getenv("MONGODB_VERSION")
 	if mdbVersion == "" {
